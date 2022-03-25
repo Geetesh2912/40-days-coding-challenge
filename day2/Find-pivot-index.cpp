0414ac4cf104791a59992e059c6f3d80acd95921
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2022-03-25 19:49:20 +0530

    Create Find-pivot-index.cpp

---
+++ day2/Find-pivot-index.cpp
@@ -0,0 +1,28 @@
+//author: Geetesh Mandaogade
+//question link: https://leetcode.com/problems/plus-one/
+//submission link: https://leetcode.com/submissions/detail/666374493/
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        //logic is leftsum*2 + current element == total sum then it is pivot
+        int lSum=0,totalSum=0;
+        for (int i=0;i<nums.size();i++)
+        {
+            totalSum=totalSum+nums[i];
+        }
+        cout<<totalSum;
+        for(int j=0;j<nums.size();j++)
+        {
+            if ((lSum*2)+nums[j]==totalSum)
+            {
+                return j;
+            }
+            else
+            {
+                lSum=lSum+nums[j];
+            }
+        }
+        return -1;
+    }
+};
