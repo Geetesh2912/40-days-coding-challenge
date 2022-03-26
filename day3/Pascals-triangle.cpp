a98ac2f38ba1cb32a794ffe029eca32c40a95525
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2022-03-26 17:27:52 +0530

    Update Pascals-triangle.cpp

--- day3/Pascals-triangle.cpp
+++ day3/Pascals-triangle.cpp
@@ -8,7 +8,7 @@ public:
         vector<vector<int>> ans(numRows); 
         for (int i=0;i<numRows;i++)
         {
-            ans[i]=vector<int>(i+1,1);
+            ans[i]=vector<int>(i+1,1);          //vector<int> (size, val) ex-> vector<int>(5,2) is basically [2,2,2,2,2] we did here because the start and end element of row is always 1, so we initialized who;e array with 1 and then did other operation on beech wale elements, we can also seperately initialize 1st and last elem instead doing this 
             for (int j=1; j<i;j++)
             {
                 ans[i][j]=ans[i-1][j-1] + ans[i-1][j];
