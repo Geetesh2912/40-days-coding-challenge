ca2b30d5e3b5085e00af02be72aa0aa224922e49
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2022-04-10 23:37:36 +0530

    Update Contains-duplicate.cpp

--- day16/Contains-duplicate.cpp
+++ day16/Contains-duplicate.cpp
@@ -1,1 +1,14 @@
+//author: Geetesh Mandaogade
+//question link: https://leetcode.com/problems/contains-duplicate/
+//submission link: https://leetcode.com/submissions/detail/676519231/
 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0; i<nums.size()-1; i++) 
+            if(nums[i]==nums[i+1])
+                return true;
+        return false;
+    }
+};
