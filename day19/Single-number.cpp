3f76b7f5a7aa4dbbe916de43a67b6a5f9888b1f3
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2022-04-11 22:08:50 +0530

    Update Single-number.cpp

--- day19/Single-number.cpp
+++ day19/Single-number.cpp
@@ -17,3 +17,22 @@ public:
         return -1;
     }
 };
+
+
+/*
+We can also use bit manipulation in this problem:
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) { 
+       int res=0;
+	   for(int i=0; i<nums.size(); i++)
+	   {
+		   res=res^nums[i];
+	   }
+	   return res;
+    }
+};
+
+
+*/
